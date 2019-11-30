fc5baccc943a9fc2b0e9e85f7798cd366d6d77cc
Greg Shackles <gshackles@gmail.com>
2019-11-30 10:49:48 -0800

    Rangers Checker: add project shell with logging

---
+++ samples/RangersPlayoffStatusChecker/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace RangersPlayoffStatusChecker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var temp = Path.GetTempPath();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File("rangers-log.txt")
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the service");
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+            }
+            finally
+            {
+                Log.Information("Stopping the service");
+
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+    }
+}
